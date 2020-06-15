a501261384bd1afda7eb86148dd839d9f28ad067
vigneshsab <vigneshsab@gmail.com>
2020-06-15 18:08:43 +0530

    Updated

---
+++ Reddit/in_shorts_scraping.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 13:21:08 2020
+
+@author: vignesh
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+seed_urls = ['https://inshorts.com/en/read/technology',
+             'https://inshorts.com/en/read/sports',
+             'https://inshorts.com/en/read/world',
+             'https://inshorts.com/en/read/entertainment',
+             'https://inshorts.com/en/read/business']
+
+def build_dataset(seed_urls):
+    news_data = []
+    for url in seed_urls:
+        news_category = url.split('/')[-1]
+        data = requests.get(url)
+        soup = BeautifulSoup(data.content, 'html.parser')
+        
+        news_articles = [{'news_headline': headline.find('span', 
+                                                         attrs={"itemprop": "headline"}).string,
+                          'news_article': article.find('div', 
+                                                       attrs={"itemprop": "articleBody"}).string,
+                          'news_category': news_category}
+                         
+                            for headline, article in 
+                             zip(soup.find_all('div', 
+                                               class_=["news-card-title news-right-box"]),
+                                 soup.find_all('div', 
+                                               class_=["news-card-content news-right-box"]))
+                        ]
+        news_data.extend(news_articles)
+        
+    df =  pd.DataFrame(news_data)
+    df = df[['news_headline', 'news_article', 'news_category']]
+    return df
+
+news_df = build_dataset(seed_urls)
+news_df.to_csv("News_scraper.csv")
